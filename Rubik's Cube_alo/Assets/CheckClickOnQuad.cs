f4df8af24cd5faced746fc4188ebf17dc146ddba
AgaMatt <heitormattosinho@gmail.com>
2016-09-08 12:49:50 -0300

    First

---
+++ Rubik's Cube_alo/Assets/CheckClickOnQuad.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckClickOnQuad : MonoBehaviour {
+	public MoveTheCube moveCube;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnMouseDown () {
+		Debug.Log (gameObject.name);
+	}
+
+	void OnMouseDrag()
+	{
+		moveCube.OnMouseDrag ();
+	}
+}
---
+++ Rubik's Cube_alo/Assets/Scripts/TestMoveTheCube.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshCollider))]
+
+
+public class TestMoveTheCube : MonoBehaviour 
+{
+	Vector3 screenPoint, scanPos, offset;
+
+	void OnMouseDown ()
+	{
+		screenPoint = Camera.main.WorldToScreenPoint (scanPos);
+
+
+		offset = scanPos - Camera.main.ScreenToWorldPoint (
+			new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+
+	}
+}
