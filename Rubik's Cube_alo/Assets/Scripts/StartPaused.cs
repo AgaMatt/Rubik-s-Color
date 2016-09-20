e34e24f5f5a3bc40ff7a1702be3881c981f0ca8c
balaringabriel <balaringabriel@gmail.com>
2016-09-20 20:08:53 -0300

    Start Paused OK

---
+++ Rubik's Cube_alo/Assets/Scripts/StartPaused.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartPaused : MonoBehaviour
+{
+
+	bool pause;
+	public GameObject pauseManager;
+
+	void Start ()
+	{
+		Time.timeScale = 0;
+		pauseManager.gameObject.SetActive (true);
+		pause = true;
+	}
+
+	public void StartButton ()
+	{
+		Time.timeScale = 1;
+		pauseManager.gameObject.SetActive (false);
+		pause = false;
+	}
+}
