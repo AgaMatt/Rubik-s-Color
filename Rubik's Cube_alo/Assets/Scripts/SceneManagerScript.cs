e988db0a7f36094bb38ae6beb411dd5ff680dc1d
balaringabriel <balaringabriel@gmail.com>
2016-11-28 18:58:40 -0200

    Almost done except music

---
+++ Rubik's Cube_alo/Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+
+	public void Play ()
+	{
+		SceneManager.LoadScene ("Game");
+	}
+
+	public void Credits ()
+	{
+		SceneManager.LoadScene ("Credits");
+	}
+
+	public void Return ()
+	{
+		SceneManager.LoadScene ("MainMenu");
+	}
+
+	public void Restart ()
+	{
+		SceneManager.LoadScene ("Game Restart");
+	}
+}
