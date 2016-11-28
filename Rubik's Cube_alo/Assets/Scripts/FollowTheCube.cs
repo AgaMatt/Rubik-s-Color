d65ff51c7f1fc7f91dbbdb9d8b999673e6904895
AgaMatt <heitormattosinho@gmail.com>
2016-11-28 16:25:14 -0200

    Credits, main menu and stuff

---
+++ Rubik's Cube_alo/Assets/Scripts/FollowTheCube.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowTheCube : MonoBehaviour {
+	public float _distance;
+	public GameObject _cube;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position = new Vector3 (_cube.transform.position.x, _cube.transform.position.y, _cube.transform.position.z - _distance);
+	}
+}
